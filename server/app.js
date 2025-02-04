57fa62245c9d1098cde85ee157de981374050058
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-02-04 18:03:20 +0200

    Added multiple API endpoints to app.js

--- server/app.js
+++ server/app.js
@@ -9,10 +9,12 @@ const sql = postgres();
 app.use("/*", cors());
 app.use("/*", logger());
 
-let questions = [];
-
 app.get("/courses", (c) => {
-  return c.json( {"courses": [ {"id": 1, "name": "Web Software Development" }, {"id": 2, "name": "Device-Agnostic Design" } ] })
+  return c.json( {"courses": [ 
+    {"id": 1, "name": "Web Software Development" }, 
+    {"id": 2, "name": "Device-Agnostic Design" } 
+  ], 
+});
 });
 
 app.get("/courses/:id", (c) => {
@@ -26,5 +28,31 @@ app.post("/courses", async (c) => {
   return c.json( {"course": { "id": 3, name}} );
 });
 
+let questions = [];
+
+app.get("/courses/:id/questions", (c) => {
+  return c.json(questions);
+});
+
+app.post("/courses/:id/questions", async (c) => {
+  const body = await c.req.json();
+  const id = questions.length + 1;
+  const question = { id, body, upvotes: 0 };
+  questions.push(question);
+  return c.json(question);
+});
+
+app.post("/courses:id/questions/:qId/upvote", (c) => {
+  const qId = Number(c.req.param("qId"));
+  const question = questions.find((q) => q.id === qId);
+  question.upvotes++;
+  return c.json(questions);
+});
+
+app.delete("/courses:id/questions/:qId", (c) => {
+  const qId = Number(c.req.param("qId"));
+  questions = questions.filter((q) => q.id !== qId);
+  return c.json(questions);
+});
 
 export default app;
