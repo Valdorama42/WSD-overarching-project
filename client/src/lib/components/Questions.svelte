a097ef9bfbc62f7aa21f96b36650084fece4a383
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-03-21 14:45:32 +0200

    removed comments

--- client/src/lib/components/Questions.svelte
+++ client/src/lib/components/Questions.svelte
@@ -18,9 +18,7 @@
   
   <h2>Questions</h2>
   <h3>Add a Question</h3>
-  <!-- When a new question is added, refreshQuestions will be triggered -->
   <QuestionForm {courseId} on:questionAdded={refreshQuestions} />
   
   <h3>Existing Questions:</h3>
-  <!-- Listen for refresh events (upvote/delete) from QuestionList -->
   <QuestionList {questions} {courseId} on:refresh={refreshQuestions} />
